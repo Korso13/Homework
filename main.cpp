7aabb7def5507b6b9f8036b9b42e2abd1af5a88e
Tim Korso <ktv91@bk.ru>
2022-04-10 19:46:48 +0300

    first commit, init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "stdint.h" // in8_t = char; int16_t = short int; int32_t = long (int); int64_t = long long;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    return 0;
+}
