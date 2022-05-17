ea35d15d2b60e01da03751268ea2c2b7f1940df7
Tim Korso <ktv91@bk.ru>
2022-05-17 20:12:08 +0300

    Homework 2.1 fix 1

--- main.cpp
+++ main.cpp
@@ -37,19 +37,18 @@ class Power
 
 class RGBA
 {
- private:
-        uint8_t m_red;
-        uint8_t m_green;
-        uint8_t m_blue;
-        uint8_t m_alpha;
- public:
-     RGBA (uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255)
-     {
-         m_red = red;
-         m_green = green;
-         m_blue = blue;
-         m_alpha = alpha; 
-     }
+private:
+    uint8_t m_red;
+    uint8_t m_green;
+    uint8_t m_blue;
+    uint8_t m_alpha;
+public:
+    RGBA(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255) :
+        m_red(red),
+        m_green(green),
+        m_blue(blue),
+        m_alpha(alpha)
+    {}
 
      void print()
      {
