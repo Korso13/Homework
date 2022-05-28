04dafc07a5145451640aaffeeb5361dbaf4c7095
Tim Korso <ktv91@bk.ru>
2022-05-29 02:14:57 +0300

    HW 2.4 Tasks 1 and 2 done

--- main.cpp
+++ main.cpp
@@ -1,162 +1,238 @@
 ﻿#include <iostream>
-#include <cmath>
-#include <cstdint>
+#include <cassert> // для assert()
+#include <vector>
 
-using namespace std;
-//============================================================================================================
-//Task 1. class Power
-//============================================================================================================
-class Power
-{
-    private:
-    double _num1;
-    double _num2;
-
-    public:
-    Power () : _num1(1), _num2(1) {}
-
-    void set()
-    {
-        cout << "Введите первое вещественное число: ";
-        cin >> _num1;
-        cout << "Введите второе вещественное число: ";
-        cin >> _num2;
-        cout << endl;
-    }
-
-    void calculate()
-    {
-        cout << "Результат возведение первого числа в степень второго: " << pow(_num1, _num2) << endl;         
-    }
-};
-
-
-//============================================================================================================
-//Task 2. class RGBA
-//============================================================================================================
-
-class RGBA
+class ArrayInt
 {
 private:
-    uint8_t m_red;
-    uint8_t m_green;
-    uint8_t m_blue;
-    uint8_t m_alpha;
-public:
-    RGBA(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255) :
-        m_red(red),
-        m_green(green),
-        m_blue(blue),
-        m_alpha(alpha)
-    {}
-
-     void print()
-     {
-         cout << "Значения красного, зелёного, синего и прозрачности: " << static_cast <int> (m_red) << "/" << static_cast <int> (m_green) << "/" << static_cast <int> (m_blue) << "/" << static_cast <int> (m_alpha) << endl; //static_cast to enable integer value output instead of char
-     }
-};
-
-//============================================================================================================
-//Task 3. Stack
-//============================================================================================================
-
-class Stack
-{
-private:
-    int st_arr[10]{0};
-    size_t st_size = 0;
+  int m_length;
+  int* m_data;
 
 public:
-    void reset()
+  ArrayInt(): m_length(0), m_data(nullptr) { }
+
+  ArrayInt(int length):
+  m_length(length)
+  {
+    assert(length >= 0);
+
+    if (length > 0) 
     {
-        for (size_t i = 0; i < st_size; i++)
-        {
-            st_arr[i] = 0;
-        }
-        st_size = 0;
+      m_data = new int[length];
+    } 
+    else 
+    {
+      m_data = nullptr;
+    }
+  }
+
+  ~ArrayInt()
+  {
+    delete[] m_data;
+  }
+
+  void erase()
+  {
+    delete[] m_data;
+
+    // Здесь нам нужно указать m_data значение nullptr, чтобы на выходе не было висячего указателя
+    m_data = nullptr;
+    m_length = 0;
+  }
+
+  int getLength() const { return m_length; }
+
+  int& operator[](int index)
+  {
+    assert(index >= 0 && index < m_length);
+    return m_data[index];
+  }
+
+// Функция resize изменяет размер массива. Все существующие элементы сохраняются. Процесс медленный
+  void resize(int newLength)
+  {
+    // Если массив уже нужной длины — return
+    if (newLength == m_length) {
+      return;
     }
 
-    bool push(int val)
-    {
-        if (st_size == 10)
-            return false;
-        else
-        {
-            st_arr[st_size] = val;
-            st_size++;
-            return true;
-        }
+    // Если нужно сделать массив пустым — делаем это и затем return
+    if (newLength <= 0) {
+      erase();
+      return;
     }
 
-    int pop()
-    {
-        if (st_size == 0)
-        {
-            cout << "Стек пуст!" << endl;
-            return 0;
-        }
-        else
-        {
-            st_size--;
-            return st_arr[st_size];
-        }
+    // Теперь знаем, что newLength >0
+    // Выделяем новый массив
+    int *data = new int[newLength];
+
+    // Затем нужно разобраться с количеством копируемых элементов в новый массив
+    // Нужно скопировать столько элементов, сколько их есть в меньшем из массивов
+    if (m_length > 0) {
+      int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+
+      // Поочередно копируем элементы
+      for (int index=0; index < elementsToCopy ; ++index) {
+        data[index] = m_data[index];
+      }
     }
 
-    void print()
-    {
-        cout << "( ";
-        for (size_t i = 0; i < st_size; i++)
-        {
-            cout << st_arr[i] << " ";
-        }
-        cout << " )" << endl;;
+    // Удаляем старый массив, так как он нам уже не нужен
+    delete[] m_data;
+
+    // И используем вместо старого массива новый! Обратите внимание, m_data указывает
+    // на тот же адрес, на который указывает наш новый динамически выделенный массив. Поскольку
+    // данные были динамически выделенные — они не будут уничтожены, когда выйдут из области видимости
+    m_data = data;
+    m_length = newLength;
+  }
+
+  void insertBefore(int value, int index)
+  {
+    // Проверка корректности передаваемого индекса
+    assert(index >= 0 && index <= m_length);
+
+    // Создаем новый массив на один элемент больше старого массива
+    int* data = new int[m_length+1];
+
+    // Копируем все элементы до index-а
+    for (int before=0; before < index; ++before) {
+      data[before] = m_data[before];
     }
+
+    // Вставляем новый элемент в новый массив
+    data [index] = value;
+
+    // Копируем все значения после вставляемого элемента
+    for (int after=index; after < m_length; ++after) {
+      data[after+1] = m_data[after];
+    }
+
+    // Удаляем старый массив и используем вместо него новый 
+    delete[] m_data;
+    m_data = data;
+    ++m_length;
+  }
+
+  void push_back(int value) { insertBefore(value, m_length); }
+
+  //=====================================================================================================
+  //Task 1. Add new methods to container class
+  //=====================================================================================================
+
+  void pop_back() //поскольку в задании сказано "для удаления", возврата у функции нет
+  {
+      int* data = new int[m_length - 1];
+      for (size_t i = 0; i < (m_length - 1); i++)
+      {
+          data[i] = m_data[i];
+      }
+      --m_length;
+      delete[] m_data;
+      m_data = data;
+  }
+
+  void pop_front()
+  {
+      int* data = new int[m_length - 1];
+      for (size_t i = 1; i < m_length; i++)
+      {
+          data[i-1] = m_data[i];
+      }
+      --m_length;
+      delete[] m_data;
+      m_data = data;
+  }
+
+  void sort() //взял сортировку пузырьком из прошлого курса по С++
+  {
+      bool trg = false;
+      int swp;
+      do
+      {
+          trg = false;   
+          for (int i = 1; i < m_length; i++)
+          {
+              if (m_data[i] < m_data[i - 1])
+              {
+                  swp = m_data[i];
+                  m_data[i] = m_data[i - 1];
+                  m_data[i - 1] = swp;
+                  trg = true;
+              }
+          }
+      } while (trg != false);
+  }
+
+  void print()
+  {
+      for (size_t i = 0; i < m_length; i++)
+      {
+          std::cout << m_data[i] << " ";
+      }
+      std::cout << std::endl;
+  }
 };
 
-
-int main()
+int main(int argc, char* argv[])
 {
     setlocale(LC_ALL, "Russian");
 
+    //=====================================================================================================
+    //Task 1. Add new methods to container class
+    //=====================================================================================================
+
     {
-        Power test;
-        test.set();
-        test.calculate();
+        ArrayInt arrayInt;
+        arrayInt.push_back(100);
+        arrayInt.push_back(0);
+        arrayInt.push_back(1);
+        arrayInt.push_back(10);
+        arrayInt.print();
+        arrayInt.sort();
+        arrayInt.print();
+        arrayInt.pop_back();
+        arrayInt.print();
+        arrayInt.pop_front();
+        arrayInt.print();
     }
+    //=====================================================================================================
+    //Task 2. Determine amount of unique vector values
+    //=====================================================================================================
+    std::cout << "Task 2" << std::endl;
 
     {
-        RGBA colour;
-        colour.print();
+        std::vector<int> vec1;
+        vec1.push_back(1); //
+        vec1.push_back(10); //
+        vec1.push_back(24); //
+        vec1.push_back(10); //
+        vec1.push_back(55);
+        vec1.push_back(124); //
+        vec1.push_back(1); //
+        vec1.push_back(86);
+        vec1.push_back(124); //
+        vec1.push_back(76);
+        vec1.push_back(24); //
+        vec1.push_back(56); //total 12. should be 8 unique. Let's check it
 
-        unsigned short int red;
-        unsigned short int green;
-        unsigned short int blue;
-        unsigned short int alpha;
-        cout << "enter red, green, blue, alpha values: ";
-        cin >> red;
-        cin >> green;
-        cin >> blue;
-        cin >> alpha; 
-        RGBA colour2(red, green, blue, alpha);
-        colour2.print();
-    }
-
-    {
-        Stack stack;
-        stack.reset();
-        stack.print();
-
-        stack.push(3);
-        stack.push(7);
-        stack.push(5);
-        stack.print();
-
-        stack.pop();
-        stack.print();
-
-        stack.pop();
-        stack.pop();
-        stack.print();
+        std::vector<int> vU;
+        bool match{ false };
+        for (size_t i = 0; i < vec1.size(); i++)
+        {
+            match = false;
+            for (size_t k = 0; k < vU.size(); k++)
+            {
+                if (vec1[i] == vU[k])
+                {
+                    match = true;
+                    break;
+                }
+            }
+            if (match == false)
+                vU.push_back(vec1[i]);
+        }
+        std::cout << "Кол - во уникальных элементов вектора: " << vU.size() << std::endl;
     }
 
     return 0;
